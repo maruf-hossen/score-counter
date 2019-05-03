838c03fd4c3ae11cd50c6bcc0e7fff30f2b9ccf3
maruf-hossen <maruf15-5445@diu.edu.bd>
2019-05-03 06:49:50 +0600

    Added Display method

--- src/main/java/com/example/mm/scorecounter/MainActivity.java
+++ src/main/java/com/example/mm/scorecounter/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.mm.scorecounter;
 
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * This method is called when the order button will be clicked
+     */
+    public void submitOrder (View view){
+        display(2);
+    }
+    private void display(int number){
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText(" " + number);
+    }
+
 }
